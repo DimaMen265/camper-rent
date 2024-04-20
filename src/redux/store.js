2c81e7f07237f3c53c2384406ba6db3b8158cf41
DimaMen265 <menzak87@gmail.com>
2024-04-20 11:30:20 +0200

    added readme

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { advertsReducer } from "./advertsSlice";
-import { filterReducer } from "./filterSlice";
 
 export const store = configureStore({
     reducer: {
         adverts: advertsReducer,
-        filter: filterReducer,
     },
 });
